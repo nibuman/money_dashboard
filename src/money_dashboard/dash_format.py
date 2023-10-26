237a1c39bdaba36136625f0b51d8ec147a2be64b
Nick Bushby <nick.bushby5@gmail.com>
2023-10-27 00:41:27 +0100

    investment table formatted

--- src/money_dashboard/dash_format.py
+++ src/money_dashboard/dash_format.py
@@ -1,4 +1,4 @@
-from dash.dash_table.Format import Format, Group, Scheme, Symbol
+from dash.dash_table.Format import Format, Group, Scheme, Symbol, Sign
 
 
 def money_format(precision: int):
@@ -12,3 +12,50 @@ def money_format(precision: int):
         symbol=Symbol.yes,
         symbol_prefix="£",
     )
+
+
+def percent_format(precision: int):
+    return Format(
+        scheme=Scheme.percentage,
+        precision=precision,
+        # group=Group.no,
+        # decimal_delimiter=".",
+        # symbol=Symbol.yes,
+        # symbol_suffix="%",
+        sign=Sign.positive,
+    )
+
+
+def conditional_format_percent_change(columns: list[str])->list[dict]:
+    conditional = []
+    for col in columns:
+        conditional.append(
+            {
+                'if': {
+                    'filter_query': '{{{col}}} < 0'.format(col=col),
+                    'column_id': col,
+                },
+                'color': 'tomato'
+            }
+        )
+        conditional.append(
+            {
+                'if': {
+                    'filter_query': '{{{col}}} = 0'.format(col=col),
+                    'column_id': col,
+                },
+                'color': 'white'
+            }
+        )
+        conditional.append(
+            {
+                'if': {
+                    'filter_query': '{{{col}}} > 0'.format(col=col),
+                    'column_id': col,
+                },
+                'color': 'green'
+            }
+        )
+    return conditional
+        
+    
--- src/money_dashboard/investments.py
+++ src/money_dashboard/investments.py
@@ -9,30 +9,27 @@ from money_dashboard import gnucash_export, dash_format
 
 
 def get_commodity_prices():
-    return (gnucash_export
-            .get_commodity_prices()
-            .ffill()
-            .drop(columns=["EUR", "GBP"])
-    )
+    return gnucash_export.get_commodity_prices().ffill().drop(columns=["EUR", "GBP"])
 
 
 def get_latest_prices():
-    return df_prices.iloc[[-1], :]
+    return df_prices.iloc[[-1], :].T.reset_index().set_axis(["commodity", "latest_price"], axis=1)
 
 
 def normalise_prices(start_year: datetime.datetime, df_prices: pd.DataFrame):
     mask = df_prices.index > start_year
     for col in df_prices.columns:
         base_price = df_prices.loc[mask, col].iloc[0]
-        df_prices.loc[mask, col] = ((df_prices.loc[mask, col] / base_price)-1)*100
+        df_prices.loc[mask, col] = ((df_prices.loc[mask, col] / base_price) - 1) * 100
     return df_prices.loc[mask]
 
 
 df_prices = get_commodity_prices()
-df_latest = get_latest_prices()
-df_normalised = normalise_prices(start_year=datetime.datetime(2020, 1, 1), df_prices=df_prices)
+latest_prices = get_latest_prices()
+df_normalised = normalise_prices(start_year=datetime.datetime(2020, 1, 1), df_prices=df_prices.copy())
 
 money = dash_format.money_format(2)
+percent = dash_format.percent_format(1)
 column_format = [
     dict(
         id=i,
@@ -43,32 +40,55 @@ column_format = [
     for i in df_prices.columns
 ][1:]
 
+
 def _commodity_performance():
-    return (df_normalised
-            .iloc[[-1], :]
-            .T
-            .reset_index()
-            .set_axis(["commodity", "change"], axis=1)
-            .sort_values(by="change", ascending=False)
-            .to_dict("records")
+    return (
+        df_normalised.iloc[[-1], :]
+        .T.reset_index()
+        .set_axis(["commodity", "change"], axis=1)
+        .sort_values(by="change", ascending=False)
+        .to_dict("records")
     )
 
 
-def _investments_table():
-    return dash_table.DataTable(
-        data=df_latest.to_dict("records"),
-        columns=column_format,
-        #page_size=10,
-        style_table={"overflowX": "auto"},
-    )
+def get_base_prices(year: datetime.datetime, df: pd.DataFrame, change_dy: int) -> pd.DataFrame:
+    mask = df.index > year
+    return df.loc[mask, :].iloc[0].T.reset_index().set_axis(["commodity", f"price_year{change_dy}"], axis=1)
+
+
+def get_commodity_data():
+    df_commodity_data = latest_prices.copy()
+    for dy in [1, 3, 5]:
+        start_year = datetime.datetime(year=2023 - dy, month=1, day=1)
+        base_prices = get_base_prices(year=start_year, df=df_prices.copy(), change_dy=dy)
+        df_commodity_data = df_commodity_data.merge(base_prices)
+        df_commodity_data[f"change_year{dy}"] = df_commodity_data.latest_price / df_commodity_data[f"price_year{dy}"]
+        df_commodity_data[f"change_year{dy}_percent"] = (df_commodity_data[f"change_year{dy}"]) - 1
+        df_commodity_data[f"annualised{dy}_percent"] = (df_commodity_data[f"change_year{dy}"] ** (1 / dy)) - 1
+    commodity_dict = df_commodity_data.fillna(0).to_dict('records')
+    return commodity_dict
+
 
 def _investment_performance_table():
     return dash_table.DataTable(
-        data=_commodity_performance(),
-        columns=[{"id": "commodity", "name": "Commodity"}, {"id": "change", "name": "Change"}],
+        data=get_commodity_data(),
+        columns=[
+            {"id": "commodity", "name": "Commodity"},
+            {"id": "latest_price", "name": "Latest Price", "type": "numeric", "format": money},
+            {"id": "change_year5_percent", "name": "5 Year (change)", "type": "numeric", "format": percent },
+            {"id": "annualised5_percent", "name": "5 Year (ann.)", "type": "numeric", "format": percent },
+            {"id": "change_year3_percent", "name": "3 Year (change)", "type": "numeric", "format": percent },
+            {"id": "annualised3_percent", "name": "3 Year (ann.)", "type": "numeric", "format": percent },
+            {'id': "change_year1_percent", "name": "1 Year (change)", "type" : "numeric", "format": percent},
+            {"id": "quantity", "name": "Quantity"},
+            {"id": "value", "name": "Value"},
+        ],
         page_size=50,
         style_table={"overflowX": "auto"},
+        style_data_conditional=dash_format.conditional_format_percent_change(['change_year3_percent', 'change_year5_percent', 'change_year1_percent'])
     )
+
+
 def _investments_graph():
     return dcc.Graph(figure={}, id="investments_overview_graph")
 
@@ -96,10 +116,9 @@ def _investment_tab():
                 dmc.Title("Investments", color="blue", size="h3"),
                 dmc.Grid(
                     [
-                        dmc.Col([_investments_table()], span=11),
                         dmc.Col([_investments_checkboxgroup()], span=2),
                         dmc.Col([_investments_graph()], span=8),
-                        dmc.Col([_investment_performance_table()], span=3),
+                        dmc.Col([_investment_performance_table()], span=8),
                     ]
                 ),
             ],
