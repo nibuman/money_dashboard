be2afbbd43bdf4fce3745099463acc566a267591
Nick Bushby <nick.bushby5@gmail.com>
2024-02-23 20:50:17 +0000

    ADDED platform information in info.py

--- pages/info.py
+++ pages/info.py
@@ -1,10 +1,13 @@
 import dash_mantine_components as dmc
 from dash import html
+import platform
 
 import utils
 import json
 
-with open(utils.DATA_PATH / "update_log.json", 'r') as f:
+DATA_OUTPUT_FORMAT = {"font-family": "monospace", "color": "gray", "fontSize": 14}
+
+with open(utils.DATA_PATH / "update_log.json", "r") as f:
     update_data = json.load(f)
 
 update_date = update_data["time"]
@@ -20,7 +23,10 @@ def create_layout():
                     [
                         dmc.Col(
                             [
-                                last_update_time(),
+                                platform_info(),
+                                os_info(),
+                                python_info(),
+                                last_update_time(),                                
                             ],
                             span=11,
                         ),
@@ -33,28 +39,33 @@ def create_layout():
     ]
 
 
-def last_update_time():
-    return html.Div(
-        [
-            html.P("Data last updated: ", style={'font-family': "monospace", 'color': 'black', 'fontSize': 14}),
-            html.P(update_date, style=data_style()),
-        ]
-    )
-
-
-def data_style():
-    return {'font-family': "monospace", 'color': 'gray', 'fontSize': 14}
-
-
 def data_file_table():
     html_table_body = []
     for num, file in enumerate(update_files, start=1):
         html_table_body.append(
             html.Tr(
                 [
-                    html.Td(num, style=data_style()),
-                    html.Td(file, style=data_style()),
+                    html.Td(num, style=DATA_OUTPUT_FORMAT),
+                    html.Td(file, style=DATA_OUTPUT_FORMAT),
                 ]
             )
         )
     return html.Table(html_table_body)
+
+def last_update_time():
+    return output_format(f"Data last updated: {update_date}")
+
+def platform_info():
+    return output_format(f"Machine architecture: {platform.machine()}")
+
+def os_info():
+    return output_format(f"Operating system: {platform.freedesktop_os_release()["PRETTY_NAME"]}")
+
+def python_info():
+    return output_format(f"Python version: {platform.python_version()}")
+
+def data_update_time():
+    return output_format(f"Date last updated: {update_date}")
+
+def output_format(text: str):
+    return html.P(text, style=DATA_OUTPUT_FORMAT)
