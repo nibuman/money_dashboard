58d0c16a74c3b0f5bf550beae80c36b7828d682d
Nick Bushby <nick.bushby5@gmail.com>
2024-03-02 00:36:14 +0000

    ADDED: modelling of target pension pot

--- pages/retirement.py
+++ pages/retirement.py
@@ -1,6 +1,6 @@
 import dash_mantine_components as dmc
 import plotly.express as px
-from dash import Input, Output, callback, dash_table, dcc
+from dash import Input, Output, callback, dash_table, dcc, html
 
 import dash_format
 import utils
@@ -10,7 +10,10 @@ summary = utils.csv_to_dict("retirement_summary.csv")
 prices = utils.csv_to_dict("retirement_price_time_series.csv")
 avg_returns = utils.csv_to_dict("retirement_average_returns.csv")
 grouped_assets = utils.csv_to_dict("retirement_grouped_by_type.csv")
+value_model = utils.csv_to_dict("retirement_value_model.csv")
 total_value = sum(float(row["value"]) for row in summary)
+
+dcc.Store(id="memory_value_model", data=value_model)
 
 
 #  Tab layout
@@ -18,21 +21,27 @@ def create_layout():
     return [
         dmc.Container(
             [
-                dmc.Title("retirements", color="blue", size="h3"),
+                dmc.Title("Retirement Investments", color="blue", size="h3"),
                 dmc.Grid(
                     [
                         dmc.Col(
                             [
-                                retirements_graph(),
-                                retirement_average_returns_table(),
+                                dmc.Col(retirements_graph(), span=12),
+                                dmc.Col(retirement_average_returns_table(), span=12),
+                                dmc.Col(retirements_radiogroup(), span=12),
+                                dmc.Col(retirement_performance_table(), span=12),
                             ],
                             span=7,
                         ),
-                        dmc.Col(retirements_performance_graph(), span=5),
-                        dmc.Col(retirements_radiogroup(), span=7),
-                        dmc.Col(retirement_performance_table(), span=11),
-                        # dmc.Col(retirements_modelling_graph(), span=7),
-                        dmc.Col(retirement_mix_pie(), span=5),
+                        dmc.Col(
+                            [
+                                dmc.Col(retirements_performance_graph(), span=12),
+                                # dmc.Col(retirement_mix_pie(), span=10),
+                                dmc.Col(retirements_modelling_parameters(), span=12),
+                            ],
+                            span=5,
+                        ),
+                        dmc.Col(retirements_modelling_graph(), span=7),
                     ]
                 ),
             ],
@@ -210,7 +219,7 @@ def retirement_mix_pie():
                 grouped_assets,
                 names="commodity_type",
                 values="type_value",
-                title=f"Current mix. Total value = £{total_value:,.0f}",
+                # title=f"Current mix. Total value = £{total_value:,.0f}",
                 hover_data="commodities",
             ),
             id="retirement_mix_pie",
@@ -218,28 +227,83 @@ def retirement_mix_pie():
     ]
 
 
-#  Retirement modelling graph
+#  Retirement modelling
+def retirements_modelling_graph():
+    for year in value_model:
+        year["target"] = 850_000
+    return dcc.Graph(
+        figure=px.line(
+            value_model,
+            x="year",
+            y=["actual_values", "target"],
+        ),
+        id="retirements_model_graph",
+    )
 
 
-# def retirements_modelling_graph():
-#     return [
-#         dcc.Graph(
-#             figure=px.line(
-#                 retirement_model.model,
-#                 x="age",
-#                 y=[
-#                     "actual_values",
-#                 ],
-#                 title="Long-Form Input",
-#             ),
-#             id="retirement_modelling_line_chart",
-#         )
-#     ]
+def retirements_modelling_parameters():
+    return dmc.Stack(
+        children=[
+            dmc.Title("Model Parameters", color="blue", size="h2"),
+            dcc.Store(id="memory_monthly_income", storage_type="local", data=0),
+            dmc.NumberInput(
+                id="retirement_monthly_income",
+                label="Monthly income (present prices):",
+                style={"width": 300},
+                type="number",
+                value=2000,
+                min=0,
+                step=10,
+                max=10000,
+            ),
+            dcc.Store(id="memory_annual_income_net", storage_type="local", data=0),
+            dcc.Store(id="memory_annual_income_gross", storage_type="local", data=0),
+            html.Div(
+                id="retirement_annual_income",
+                children=[
+                    dmc.Text("Annual income (net, present value): £0"),
+                    dmc.TextInput("Annual income (gross, present value): £0"),
+                ],
+            ),
+            dcc.Store(id="memory_removal_rate", storage_type="local", data=0),
+            dmc.NumberInput(
+                id="retirement_removal_rate",
+                label="Removal rate (%)",
+                style={"width": 300},
+                value=3,
+                min=0.1,
+                step=0.1,
+                precision=1,
+                max=10,
+            ),
+            dcc.Store(id="memory_income_sum", storage_type="local", data=0),
+            html.Div(
+                id="retirement_income_sum",
+                children=[
+                    dmc.Text("Required sum to produce income: £0"),
+                ],
+            ),
+            dcc.Store(id="memory_lump_sum", storage_type="local", data=0),
+            dmc.NumberInput(
+                id="retirement_lump_sum",
+                label="Lump sum",
+                style={"width": 300},
+                value=100_000,
+                min=0,
+                step=1_000,
+            ),
+            dcc.Store(id="memory_total_sum", storage_type="local", data=0),
+            html.Div(
+                id="retirement_total_sum",
+                children=[
+                    dmc.Text("Total sum required: £0"),
+                ],
+            ),
+        ],
+    )
 
 
 #  Radio buttons
-
-
 def retirements_radiogroup():
     return [
         dmc.RadioGroup(
@@ -261,6 +325,85 @@ def retirements_radios() -> list[dmc.Radio]:
 
 
 #  Callbacks
+@callback(
+    Output(component_id="retirement_income_sum", component_property="children"),
+    Output(component_id="memory_income_sum", component_property="data"),
+    Input(component_id="memory_annual_income_gross", component_property="data"),
+    Input(component_id="memory_removal_rate", component_property="data"),
+)
+def update_income_sum(gross_income, removal_rate):
+    income_sum = int(gross_income / (removal_rate / 100))
+    return (
+        [
+            dmc.Text(f"Sum required to provide income: £{income_sum}"),
+        ],
+        income_sum,
+    )
+
+
+@callback(
+    Output(component_id="retirement_total_sum", component_property="children"),
+    Input(component_id="memory_total_sum", component_property="data"),
+)
+def update_total_sum(total_sum):
+    return [
+        dmc.Text(f"Total sum required: £{total_sum}"),
+    ]
+
+
+@callback(
+    Output(component_id="memory_monthly_income", component_property="data"),
+    Input(component_id="retirement_monthly_income", component_property="value"),
+)
+def store_monthly_income(monthly_income):
+    return int(monthly_income)
+
+
+@callback(
+    Output(component_id="memory_removal_rate", component_property="data"),
+    Input(component_id="retirement_removal_rate", component_property="value"),
+)
+def store_removal_rate(removal_rate):
+    return float(removal_rate)
+
+
+@callback(
+    Output(component_id="memory_lump_sum", component_property="data"),
+    Output(component_id="memory_total_sum", component_property="data"),
+    Input(component_id="memory_income_sum", component_property="data"),
+    Input(component_id="retirement_lump_sum", component_property="value"),
+)
+def store_lump_sum(income_sum, lump_sum):
+    lump_sum = int(lump_sum)
+    return (lump_sum, lump_sum + income_sum)
+
+
+@callback(
+    Output(component_id="retirement_annual_income", component_property="children"),
+    Input(component_id="memory_annual_income_net", component_property="data"),
+    Input(component_id="memory_annual_income_gross", component_property="data"),
+)
+def update_annual_income(net_income, gross_income):
+    return [
+        dmc.Text(f"Annual income (net, present value): £{net_income}"),
+        dmc.Text(f"Annual income (gross, present value): £{gross_income}"),
+    ]
+
+
+@callback(
+    Output(component_id="memory_annual_income_net", component_property="data"),
+    Output(component_id="memory_annual_income_gross", component_property="data"),
+    Input(component_id="memory_monthly_income", component_property="data"),
+)
+def store_annual_income(monthly_income):
+    annual_income_net = int(monthly_income) * 12
+    LOWER_RATE_TAX = 0.2
+    PERSONAL_ALLOWANCE = 12_000
+    if annual_income_net > PERSONAL_ALLOWANCE:
+        annual_income_gross = ((annual_income_net - PERSONAL_ALLOWANCE) / (1 - LOWER_RATE_TAX)) + PERSONAL_ALLOWANCE
+    else:
+        annual_income_gross = annual_income_net
+    return (annual_income_net, annual_income_gross)
 
 
 @callback(
@@ -295,3 +438,17 @@ def radio_button_actions(sort_col):
         year = sort_col.removeprefix("radio_year").removesuffix("_percent")
         col = f"annualised{year}_percent"
     return update_bar_chart(col), update_table(col), update_tooltips(col)
+
+
+@callback(
+    Output(component_id="retirements_model_graph", component_property="figure"),
+    Input(component_id="memory_total_sum", component_property="data"),
+)
+def update_model_graph(target):
+    for year in value_model:
+        year["target"] = target
+    return px.line(
+        value_model,
+        x="year",
+        y=["actual_values", "target"],
+    )
