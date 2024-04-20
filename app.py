5bd62929078e4b02dbfc451ffe53279c591f3121
Nick Bushby <nick.bushby5@gmail.com>
2024-04-20 22:50:17 +0100

    Separate tabs created for retirement model and investments

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash_mantine_components as dmc
 from dash import Dash, dcc
 
 # from money_dashboard import investments, retirement
-from pages import assets, info, investments, retirement
+from pages import assets, info, investments, retirement, retirement_model
 
 # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
@@ -14,14 +14,16 @@ app.layout = dmc.Tabs(
             [
                 dmc.Tab("Assets", value="1"),
                 dmc.Tab("Investments", value="2"),
-                dmc.Tab("Retirement", value="3"),
-                dmc.Tab("Info", value="4"),
+                dmc.Tab("Retirement Investments", value="3"),
+                dmc.Tab("Retirement Model", value="4"),
+                dmc.Tab("Info", value="5"),
             ]
         ),
         dmc.TabsPanel(assets.create_layout(), value="1"),
         dmc.TabsPanel(investments.create_layout(), value="2"),
         dmc.TabsPanel(retirement.create_layout(), value="3"),
-        dmc.TabsPanel(info.create_layout(), value="4"),
+        dmc.TabsPanel(retirement_model.create_layout(), value="4"),
+        dmc.TabsPanel(info.create_layout(), value="5"),
         dcc.Store(id="sorted_data"),
     ],
     value="1",
@@ -32,3 +34,4 @@ if __name__ == "__main__":
     # Need these settings for the server to be visible to other computers on the network
     # app.run_server(host="0.0.0.0", port="8050")
     app.run()
+
